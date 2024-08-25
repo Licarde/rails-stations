4f71c7cff61c2f46b43e3910bbdf9bcfb8259df5
Rio Hakamata <kinokohakamata@gmail.com>
2024-08-25 14:16:11 +0900

    Station 9

--- app/controllers/admin/schedules_controller.rb
+++ app/controllers/admin/schedules_controller.rb
@@ -1,2 +1,24 @@
 class Admin::SchedulesController < ApplicationController
+  def index
+  end
+  
+  def create
+  end
+
+  def edit
+    @schedule = Schedule.find(params[:id])
+  end
+
+  def update
+    schedule = Schedule.find(params[:id])
+    schedule.start_time = params[:schedule][:start_time]
+    #schedule.end_time = params[:schedule][:end_time]
+    schedule.save
+    redirect_to("/admin/schedules")
+  end
+
+  def destroy
+    Schedule.find(params[:id]).destroy
+    redirect_to("/admin/schedules")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'sheets/sheets'
   get "movies" => "movies#index"
   get "movies/search" => "movies#search"
+  get "movies/:id" => "movies#show"
+  
+  get "sheets" => "sheets#index"
+
   post "admin/movies" => "admin/movies#create"
   get "admin/movies" => "admin/movies#index"
   get "admin/movies/new" => "admin/movies#new"
@@ -9,6 +12,12 @@ Rails.application.routes.draw do
   post "admin/movies/:id" => "admin/movies#destroy"
   delete "admin/movies/:id" => "admin/movies#destroy"
   put "admin/movies/:id" => "admin/movies#update"
-  get "sheets" => "sheets#index"
-  get "movies/:id" => "movies#show"
+
+  get "admin/schedules" => "admin/schedules#index"
+  get "admin/schedules/:id" => "admin/schedules#edit"
+  #get "admin/schedules/:id/schedules/new" => "admin/schedules#new"
+  put "admin/schedules/:id" => "admin/schedules#update"
+  delete "admin/schedules/:id" => "admin/schedules#destroy"
+  #post "admin/schedules/:id" => "admin/schedules#destroy"
+  
 end
